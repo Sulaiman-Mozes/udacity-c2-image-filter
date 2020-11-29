2b8869ddb5b1de5a9763012b62b9a9a46fba42ee
Sulaiman-Mozes <sulaimanmoses13@gmail.com>
2020-11-29 19:02:30 +0300

    - add req and res types

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+// import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -15,7 +16,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 
   // GET /filteredimage?image_url={{URL}}
-  app.get( "/filteredimage/", async ( req, res ) => {
+  app.get( "/filteredimage/", async ( req: Request, res: Response ) => {
     const { image_url } = req.query;
     if (!image_url) {
       return res.status(400).send({ message: 'image url is required' });
@@ -23,7 +24,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     filterImageFromURL(image_url).then((path: string) => {
       res.sendFile(path)
       res.on('finish', () => deleteLocalFiles([path]));
-    }).catch((err) => {
+    }).catch((err: string) => {
       return res.status(400).send({ message: err });
     })
   } );
@@ -31,7 +32,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async (  req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
